0eec8343819a5bd872b7699bf72f913dff94d9bc
kingdev <kingdev5809@gmail.com>
2023-02-23 23:30:54 +0500

    ff

--- src/Redux/Actions/ModalAction.js
+++ src/Redux/Actions/ModalAction.js
@@ -1,27 +1,27 @@
-import {
-  TEACHERS_FETCHED,
-  TEACHER_FETCHING,
-  TEACHER_FETCHING_ERROR,
-} from "../Constants/ModalContants.js";
-import * as AdminApi from "../../api/AdminRequest";
+// import {
+//   TEACHERS_FETCHED,
+//   TEACHER_FETCHING,
+//   TEACHER_FETCHING_ERROR,
+// } from "../Constants/ModalContants.js";
+// import * as AdminApi from "../../api/AdminRequest";
 
-export const teacher =
-  (name, surname ,email ,phone_number,address,photo) => async (dispatch) => {
-    dispatch({ type: TEACHER_FETCHING });
-    try {
-      const { data } = await AdminApi.rektor({
-        name, surname ,email ,phone_number,address,photo
-      });
-      dispatch({ type: TEACHERS_FETCHED, payload: data.teachers });
-      localStorage.setItem("teachers", JSON.stringify(data.teachers));
-    } catch (error) {
-      dispatch({
-        type: TEACHER_FETCHING_ERROR,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+// export const teacher =
+//   (name, surname ,email ,phone_number,address,photo) => async (dispatch) => {
+//     dispatch({ type: TEACHER_FETCHING });
+//     try {
+//       const { data } = await AdminApi.rektor({
+//         name, surname ,email ,phone_number,address,photo
+//       });
+//       dispatch({ type: TEACHERS_FETCHED, payload: data.teachers });
+//       localStorage.setItem("teachers", JSON.stringify(data.teachers));
+//     } catch (error) {
+//       dispatch({
+//         type: TEACHER_FETCHING_ERROR,
+//         payload:
+//           error.response && error.response.data.message
+//             ? error.response.data.message
+//             : error.message,
+//       });
+//     }
+//   };
 
--- src/Redux/Reducers/ModalReducers.js
+++ src/Redux/Reducers/ModalReducers.js
@@ -1,38 +1,32 @@
-import {
-  TEACHERS_CREATED,
-  TEACHERS_FETCHED,
-  TEACHER_FETCHING,
-  TEACHER_FETCHING_ERROR,
-} from "../Constants/ModalContants.js";
+// import {
+//   TEACHERS_CREATED,
+//   TEACHERS_FETCHED,
+//   TEACHER_FETCHING,
+//   TEACHER_FETCHING_ERROR,
+// } from "../Constants/ModalContants.js";
 
-// const initialState = {
-//   teachers: [],
-//   teachersLoadingStatus: "false",
-//   error: "",
-//   students: [],
+
+// const reducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case TEACHER_FETCHING:
+//       return {
+//         ...state,
+//         loading: "true",
+//       };
+//     case TEACHERS_FETCHED:
+//       return {
+//         ...state,
+//         teachers: action.payload,
+//       };
+//     case TEACHER_FETCHING_ERROR:
+//       return {
+//         ...state,
+//         error: action.payload,
+//       };
+//     case TEACHERS_CREATED:
+//       return {
+//         ...state,
+//         teachers: [...state.teachers, action.payload],
+//       };
+//   }
 // };
-
-const reducer = (state = {}, action) => {
-  switch (action.type) {
-    case TEACHER_FETCHING:
-      return {
-        ...state,
-        loading: "true",
-      };
-    case TEACHERS_FETCHED:
-      return {
-        ...state,
-        teachers: action.payload,
-      };
-    case TEACHER_FETCHING_ERROR:
-      return {
-        ...state,
-        error: action.payload,
-      };
-    // case TEACHERS_CREATED:
-    //   return {
-    //     ...state,
-    //     teachers: [...state.teachers, action.payload],
-    //   };
-  }
-};
