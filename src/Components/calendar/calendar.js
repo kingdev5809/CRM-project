af2ae3673be602e650b5fe389f2170851ca200ff
kingdev <kingdev5809@gmail.com>
2023-02-24 18:59:11 +0500

    delete

--- src/Components/calendar/calendar.js
+++ src/Components/calendar/calendar.js
@@ -39,7 +39,7 @@ export default function Calendar() {
     }else{
       setIsMobile(false)
     }
-    console.log(isMobile);
+    
   }
   
 
--- src/Layout/userStudent/Homework Page/StudentHomework.js
+++ src/Layout/userStudent/Homework Page/StudentHomework.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import Navbar from '../../../Components/Navbar'
-import Groups from './Groups'
-import Homeworks from './Homeworks'
-
+import React, { useEffect, useRef, useState } from "react";
+import Navbar from "../../../Components/Navbar";
+import Groups from "./Groups";
+import Homeworks from "./Homeworks";
 
 function Homework() {
+  const windowSize = useRef([window.innerWidth]);
+  const [visibleHomework, setVisibleHomework] = useState(true);
+
+  useEffect(() => {
+    if (windowSize.current[0] < 1050) {
+      setVisibleHomework(false);
+    } else {
+      setVisibleHomework(true);
+    }
+  });
+
   return (
-    <div className='flex'>
-      <Navbar/>
+    <div className="flex">
+      <Navbar />
       <div className="homeworkPage container">
-        <Groups/>
-        <Homeworks/>
+        <Groups />
+       {visibleHomework?  <Homeworks /> : ''}
       </div>
     </div>
-  )
+  );
 }
 
-export default Homework
+export default Homework;
